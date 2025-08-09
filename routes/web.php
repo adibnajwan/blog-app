c2eaa832b30311cd24f62c34b7dc3b88f83cc4bf
adibnajwan <adibnajwan@gmail.com>
2025-08-09 07:05:15 +0700

    feat: routes for posts

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,10 +23,13 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth'])->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Rute untuk CRUD Post, hanya bisa diakses setelah login
+    Route::resource('posts', PostController::class); 
 });
 
 require __DIR__.'/auth.php';
